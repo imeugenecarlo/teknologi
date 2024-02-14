ae01b7cfff65757a847a49cf986cda0f21097321
Eugene B. <beugenecarlo@gmail.com>
2024-02-14 19:45:59 +0100

    update python 2

--- TCPpython 2.py
+++ TCPpython 2.py
@@ -1,22 +1,29 @@
 from socket import *
+import threading
 
 def handleClient(connectionSocket,addr):
     print(addr[0])
+    keep_communacating = True
 
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode)
+    while keep_communacating:
+        sentence = connectionSocket.recv(1024).decode()
+        print(sentence)
+        if sentence.strip()== "close":
+            keep_communacating = False
+        else:
+            capitalizedSentence = sentence.upper()
+            connectionSocket.send(capitalizedSentence.encode)
+           
     connectionSocket.close()
-    print(addr)
-    print(sentence)
 
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.bind((serverName, serverPort))
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print('From server: ', modifiedSentence.decode())
-clientSocket.close()
+
+print('From server: ')
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    threading.Thread(target=handleClient, args=(connectionSocket, addr)).start()
 
     
